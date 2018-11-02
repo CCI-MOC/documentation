ce6eb9957ec02ea97a20eac3fdb9bdc08c0823ed
hgibson <hgibson@redhat.com>
2018-11-02 15:02:30 -0400

    Reran spinx-quickstart so correct directories would be generated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,13 +16,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-from recommonmark.parser import CommonMarkParser
-
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
-source_suffix = ['.rst', '.md']
 
 # -- Project information -----------------------------------------------------
 
@@ -46,11 +39,16 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -64,11 +62,11 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
+# This pattern also affects html_static_path and html_extra_path .
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = None
+pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -156,23 +154,10 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
+from recommonmark.parser import CommonMarkParser
 
-# -- Options for Epub output -------------------------------------------------
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
-# Bibliographic Dublin Core info.
-epub_title = project
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-
-# -- Extension configuration -------------------------------------------------
+source_suffix = ['.rst', '.md']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,16 +1,15 @@
 .. MOC documentation master file, created by
-   sphinx-quickstart on Tue Oct  2 15:38:22 2018.
+   sphinx-quickstart on Fri Nov  2 15:01:09 2018.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Mass Open Cloud
+Welcome to MOC's documentation!
 ===============================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
-   Home <home.md>
 
 
 Indices and tables
